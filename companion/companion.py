1ccb8363ef70d0e09322ed8934c9a18808f6a864
pippcat <sendyourspamhere+github@posteo.de>
2018-07-24 23:53:18 +0200

    small improvements

--- companion/companion.py
+++ companion/companion.py
@@ -2,14 +2,22 @@
 #-*- coding: utf-8 -*-
 
 import image_manipulator
+import drawing_simulator
 
 print("Drawing Robot Companion Program")
 print("===============================\n")
-imagename = input("Please put your image into the images subfolder and enter the filename with extension but without path here: ")
-im = image_manipulator.open_image(imagename)
-res = image_manipulator.resize_image(im, 800)
-edge = image_manipulator.edge_detector(res)
-inv = image_manipulator.inverter(edge)
-image_manipulator.save_file('out', inv)
-image_manipulator.show_results(im, res, edge, inv)
 
+def process_image():
+    imagename = input("Please put your image into the images subfolder and enter the filename with extension but without path here: ")
+    im = image_manipulator.open_image(imagename)
+    res = image_manipulator.resize_image(im, 800)
+    edge = image_manipulator.edge_detector(res)
+    inv = image_manipulator.inverter(edge)
+    image_manipulator.save_file('out', inv)
+    image_manipulator.show_results(im, res, edge, inv)
+
+def draw_simulation():
+    scenery = drawing_simulator.setup_scenery()
+    drawing_simulator.dummy(scenery['angleLabel'], scenery['innerArm'], scenery['outerArm'], scenery['innerLength'],scenery['outerLength'])
+
+draw_simulation()
