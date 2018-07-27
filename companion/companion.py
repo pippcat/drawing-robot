59c16ab87a6128f80105114964377c13686e8b8b
pippcat <sendyourspamhere+github@posteo.de>
2018-07-27 04:08:17 +0200

    setting up things according to size of image

--- companion/companion.py
+++ companion/companion.py
@@ -3,13 +3,14 @@
 
 import image_manipulator
 import drawing_simulator
+import numpy as np
 
 print("Drawing Robot Companion Program")
 print("===============================\n")
 
 def process_image():
     imagename = input("Please put your image into the images subfolder and enter the filename with extension but without path here: ")
-    imagewidth = input("Which width in pixel should the processed image have?: ")
+    imagewidth = input("Which width in pixel should the processed image have (recommendation: 50 .. 100)? ")
     im = image_manipulator.open_image(imagename)
     res = image_manipulator.resize_image(im, int(imagewidth))
     edge = image_manipulator.edge_detector(res)
@@ -22,7 +23,9 @@ def process_image():
 def draw_simulation(image):
     print("setting up simulator")
     scenery = drawing_simulator.setup_scenery(image)
-    drawing_simulator.draw_image(scenery['alphaLabel'], scenery['betaLabel'], scenery['innerArm'], scenery['outerArm'], scenery['innerLength'],scenery['outerLength'],scenery['image'], scenery['image_scale'], scenery['image_shift_x'])
+    drawing_simulator.draw_image(scenery['alphaLabel'], scenery['betaLabel'], scenery['innerArmLabel'], scenery['outerArmLabel'],scenery['innerArm'], scenery['outerArm'], scenery['innerLength'],scenery['outerLength'],scenery['image'], scenery['image_scale'])
 
-image = process_image()
+image = process_image() # either .. 
+#print("skipping image processing") # ..or
+#image = image_manipulator.image_as_array('out.png', 0.9)
 draw_simulation(image)
--- companion/drawing_simulator.py
+++ companion/drawing_simulator.py
@@ -13,46 +13,54 @@ from skimage.io import imread
 
 innerArmLength = 100
 outerArmLength = 80
+ArmLength = innerArmLength + outerArmLength
+origin = {'x':ArmLength/6,'y':ArmLength/6} # origin of robot with respect to (0/0)
 innerPos = vector(0,0,0) # origin of inner arm
 innerLength = vector(innerArmLength,0,0) # length of inner arm
 outerPos = innerPos + innerLength # origin of outer arm
 outerLength = vector(outerArmLength,0,0) # length of outer arm
 
-def draw_line(axis,position,linecolor):
-    if axis == "y":
-        #return box(pos=position, length=400, height=0.2, width=0.2)   
-        return curve(vector(position)-vector(200,0,0), vector(position)+vector(200,0,0), radius=0.5, color=linecolor)
+def draw_line(axis,position,linecolor,gridX,gridY): # draws grid for orientation
     if axis == "x":
-        return curve(vector(position)-vector(0,100,0), vector(position)+vector(0,100,0), radius=0.5, color=linecolor)
+        return curve(vector(position), vector(position)+vector(gridX,0,0), radius=0.5, color=linecolor)
+    if axis == "y":
+        return curve(vector(position), vector(position)+vector(0,gridY,0), radius=0.5, color=linecolor)
 
 def setup_scenery(image):
-    scene = canvas(title='Drawing robot simulation', width=1600, height=900) 
-    
+    image_scale = 1.5*image.shape[0]/(ArmLength)
+    gridX = image.shape[0]/image_scale+origin['x']
+    gridY = image.shape[1]/image_scale+origin['y']
+    scene = canvas(title='Drawing robot simulation', width=1600, height=900, center=vector(gridX/2,ArmLength/6,0), forward=(vector(0,0,-1)), range=gridX)
     #Create virtual environment:
     #first we create the arrows which represent the arms:
     innerArm = arrow(pos=(innerPos), axis=(innerLength), shaftwidth=4, headwidth=6, color=color.blue)
     outerArm = arrow(pos=(outerPos), axis=(outerLength), shaftwidth=4, headwidth=6, color=color.red)
     #and the now the labels
-    alphaLabel = label (text = 'First servo angle is: 0', pos=(vector(0,-20,0)), height=15, box=False)
-    betaLabel = label (text = 'Second servo angle is: 0', pos=(vector(0,-40,0)), height=15, box=False)
-    maxRange = ring(pos=vector(0,0,0), axis=vector(0,0,1), radius=innerArmLength+outerArmLength, thickness=1, color=color.cyan)
-    for i in range(0,205,5): # drawing area, pixelated
+    alphaLabel = label (text = 'First servo angle is: 0', pos=(vector(-gridX/2,gridY/2,0)), height=15, box=False)
+    betaLabel = label (text = 'Second servo angle is: 0', pos=(vector(-gridX/2,gridY/4,0)), height=15, box=False)
+    originLabel = label(text="("+str(int(origin['x']))+"/"+str(int(origin['y']))+")", pos=(vector(origin['x'],origin['y'],0)), height=15, box=False, color=color.green)
+    endLabel = label(text="("+str(int(image.shape[0]/image_scale+origin['x']))+"/"+str(int(image.shape[1]/image_scale+origin['y']))+")", pos=(vector(image.shape[0]/image_scale+origin['x'],image.shape[1]/image_scale+origin['y'],0)), height=15, box=False, color=color.green)
+    innerArmLabel = label(text='(0/0)', pos=(vector(0,0,0)), height=15, box=False, color=color.blue)
+    outerArmLabel = label(text='('+ str(outerArm.pos.x) + '/'+ str(outerArm.pos.y)+ ')', pos=outerArm.pos, height=15, box=False, color=color.red)
+    maxRange = ring(pos=vector(0,0,0), axis=vector(0,0,1), radius=ArmLength, thickness=1, color=color.cyan)
+    for i in range(0,int(gridY + gridY / 50), int(gridY / 50)): # drawing y grid lines
         if i%50 == 0:
-            draw_line('y',vector(0,i,0),color.white)
+            draw_line('x',vector(0,i,0),color.white,gridX,gridY) # every 50th line is white
         else:
-            draw_line('y',vector(0,i,0),color.gray(0.2))
-    for i in range(-200,205,5): # drawing area, pixelated
+            draw_line('x',vector(0,i,0),color.gray(0.2),gridX,gridY) # the others are gray
+    for i in range(0,int(gridX + gridX / 50),int(gridX / 50)): # drawing x grid lines
         if i%50 == 0:
-            draw_line('x',vector(i,100,0),color.white)
+            draw_line('y',vector(i,0,0),color.white,gridX,gridY)
         else:
-            draw_line('x',vector(i,100,0),color.gray(0.2))
-    image_scale = image.shape[0]/(innerArmLength+outerArmLength)
-    image_shift_x = (image.shape[0]/image_scale)/2
+            draw_line('y',vector(i,0,0),color.gray(0.2),gridX,gridY)
+    #image_scale = image.shape[0]/(innerArmLength+outerArmLength)
+    #image_shift_x = (image.shape[0]/image_scale)/2
+#    image_shift_x = image.shape[0]/image_scale/2
     for ix in range(image.shape[0]):
         for iy in range(image.shape[1]):
             if image[ix,iy] < 0.5:
-                box(pos=vector((ix/image_scale-image_shift_x),iy/image_scale,0.1), length=1, height=1, width=1, color=color.green)
-    return {'alphaLabel':alphaLabel, 'betaLabel':betaLabel, 'innerArm':innerArm, 'outerArm':outerArm, 'innerLength':innerLength, 'outerLength':outerLength, 'image':image, 'image_scale':image_scale, 'image_shift_x':image_shift_x}
+                box(pos=vector(ix/image_scale+origin['x'],iy/image_scale+origin['y'],0.1), length=1, height=1, width=1, color=color.green)
+    return {'alphaLabel':alphaLabel, 'betaLabel':betaLabel, 'innerArmLabel':innerArmLabel,'outerArmLabel':outerArmLabel,'innerArm':innerArm, 'outerArm':outerArm, 'innerLength':innerLength, 'outerLength':outerLength, 'image':image, 'image_scale':image_scale}
 
 l1 = innerArmLength #Length of link 1
 l2 = outerArmLength #length of link 2
@@ -90,46 +98,54 @@ def find_pixel(image):
     return False,False
 
 def find_adjacent_pixel(image,ix,iy):
-    for ay in range(iy-1,ix+1,1):
-        for ax in range(ix-1,ix+1,1):
-            print("ix: "+str(ix)+", iy: "+str(iy)+", ax: "+str(ax)+", ay: "+str(ay)+", image: "+str(image[ax,ay]))
+    ax=ix
+    for ay in range(iy+1,iy-1,-1):
+        if image[ax,ay] < 0.5:
+            image[ax,ay] = 1
+            return ax,ay
+        for ax in range(ix+1,ix-1,-1):
+            #print("fap: ix: "+str(ix)+", iy: "+str(iy)+", ax: "+str(ax)+", ay: "+str(ay)+", image: "+str(image[ax,ay]))
             if image[ax,ay] < 0.5:
                 image[ax,ay] = 1
                 return ax,ay
     return False,False
 
-def draw_image(alphaLabel,betaLabel,innerArm,outerArm,innerLength,outerLength,image,image_scale,image_shift_x):# dummy function
+def draw_image(alphaLabel,betaLabel,innerArmLabel,outerArmLabel,innerArm,outerArm,innerLength,outerLength,image,image_scale):# dummy function
     ax=ix=ay=ix=False
     alpha=beta=0
     while (1==1):
-        rate(20) #refresh rate required for VPython
+        rate(60) #refresh rate required for VPython
         ix,iy = find_pixel(image)
-        if ix and iy:
-                alpha, beta = getangles(ix/image_scale-image_shift_x,iy/image_scale)
+        if (ix and iy):
+                alpha, beta = getangles((ix/image_scale+origin['x']),iy/image_scale+origin['x'])
+                #print("### alpha: "+str(alpha)+"; beta: "+str(beta))
                 if alpha != 0 and beta != 0:
-                    drive_arms(alpha,beta,ix,iy,innerArm,outerArm,innerLength,outerLength,alphaLabel,betaLabel,image_scale,image_shift_x)
-                    c = curve(vector(ix/image_scale - image_shift_x,iy/image_scale,1))
+                    drive_arms(alpha,beta,ix,iy,innerArm,outerArm,innerLength,outerLength,alphaLabel,betaLabel,innerArmLabel,outerArmLabel,image_scale)
+                    c = curve(vector(ix/image_scale+origin['x'],iy/image_scale+origin['y'],1), color=color.magenta, radius=0.5)
                     ax,ay = find_adjacent_pixel(image,ix,iy)
-                    print('ax: ' + str(ax)+ '; ay: ' + str(ay))
+                    #print('### ax: ' + str(ax)+ '; ay: ' + str(ay))
                     while (ax and ay):
-                        c.append(vector(ax/image_scale - image_shift_x,ay/image_scale,1))
-                        alpha, beta = getangles(ax/image_scale-image_shift_x,ay/image_scale)
+                        c.append(vector(ax/image_scale+origin['x'],ay/image_scale+origin['y'],1))
+                        alpha, beta = getangles(ax/image_scale+origin['x'],ay/image_scale+origin['y'])
                         if alpha != 0 and beta != 0:
-                            drive_arms(alpha,beta,ax,ay,innerArm,outerArm,innerLength,outerLength,alphaLabel,betaLabel,image_scale,image_shift_x)
+                            drive_arms(alpha,beta,ax,ay,innerArm,outerArm,innerLength,outerLength,alphaLabel,betaLabel,innerArmLabel,outerArmLabel,image_scale)
                         ax,ay = find_adjacent_pixel(image,ax,ay)
+                        #print('###### ax: ' + str(ax)+ '; ay: ' + str(ay))
         else:
             print("Nothing left to draw!")
             return
-        print("ix: " + str(ix) + ", iy: " + str(iy) + ", ax: " + str(ax) + ", ay: " + str(ay) + ", alpha: " + str(alpha) + ", beta: " + str(beta))
-        time.sleep(0.3)
+        #print("ix: " + str(ix) + ", iy: " + str(iy) + ", ax: " + str(ax) + ", ay: " + str(ay) + ", alpha: " + str(alpha) + ", beta: " + str(beta))
     
-def drive_arms(alpha, beta,x,y,innerArm,outerArm,innerLength,outerLength,alphaLabel,betaLabel,image_scale,image_shift_x):
-    #box(pos=vector(x/image_scale-image_shift_x,y/image_scale,1), length=1, height=1, width=1, color=color.cyan)
-    innerArm.axis = innerLength # reset arm to 0 deg
+def drive_arms(alpha, beta,x,y,innerArm,outerArm,innerLength,outerLength,alphaLabel,betaLabel,innerArmLabel,outerArmLabel,image_scale):
+    #box(pos=vector(x/image_scale-image_shift_x,y/image_scale,1), length=1, height=1, width=1, color=color.cyan) innerArm.axis = innerLength # reset arm to 0 deg
+    innerArm.axis = innerLength
     innerArm.axis = rotate(innerArm.axis, angle=alpha, axis=(vector(0,0,1))) # rotate it using alpha
     outerArm.pos=(innerPos + innerArm.axis) # calculate starting point of second arm
     outerArm.axis = innerArm.axis # set orientation equal to first arm
     outerArm.length = outerArmLength # reset length of arm
     outerArm.axis = rotate(outerArm.axis, angle=beta, axis=(vector(0,0,1))) # rotate it
-    alphaLabel.text = "First servo angle is: " + str(np.rad2deg(alpha))
-    betaLabel.text = "Second servo angle is: " + str(np.rad2deg(beta))
+    alphaLabel.text = "First servo angle is: " + str(int(np.rad2deg(alpha)))
+    betaLabel.text = "Second servo angle is: " + str(int(np.rad2deg(beta)))
+    outerArmLabel.text = '('+ str(int(outerArm.pos.x)) + '/'+ str(int(outerArm.pos.y))+ ')'
+    outerArmLabel.pos = outerArm.pos
+    time.sleep(.05)
