8cf3397fc08b362a16cbe1f4137d6f3d7a86eedf
Tommy <tommy.schoenherr@posteo.de>
2018-08-30 19:09:43 +0200

    small commit

--- drawing-robot-headless.py
+++ drawing-robot-headless.py
@@ -15,7 +15,6 @@ from bokeh.io import curdoc
 from bokeh.models import Panel, Button
 from bokeh.models.widgets import Tabs, Div, TextInput
 from bokeh.layouts import column, row, WidgetBox
-from time import sleep
 # own stuff:
 import helper
 import imageProcessor.imageProcessor
@@ -29,7 +28,7 @@ config.read('config.ini')
 debug = config['general'].getboolean('debug')
 originX = config['general'].getint('originX')
 originY = config['general'].getint('originY')
-imageScaleFactor = config['general'].getint('imageScale')
+imageScale = config['general'].getint('imageScale')
 # arms:
 innerArmLength = config['arms'].getint('innerArmLength')
 innerArmChannel = config['arms'].getint('innerArmChannel')
@@ -50,33 +49,31 @@ skipProcessing = config['imageprocessor'].getboolean('skipProcessing')
 outputFilename = config['imageprocessor']['outputFilename']
 treshold = config['imageprocessor'].getfloat('treshold')
 # raspi
-raspiSwitchedOn = config['raspi'].getboolean('switchedOn')
 frequency = config['raspi'].getint('frequency')
 waitTimeNear = config['raspi'].getfloat('waitTimeNear')
 waitTimeNew = config['raspi'].getfloat('waitTimeNew')
 waitTimePen = config['raspi'].getfloat('waitTimePen')
 # simulator
-simulatorSwitchedOn = config['simulator'].getboolean('switchedOn')
 browser = config['simulator']['browser']
 sizeX = config['simulator'].getint('sizeX')
 sizeY = config['simulator'].getint('sizeY')
 penWidth = config['simulator'].getint('penWidth')
 
 ### setting up some meaningful dictionaries:
-arms = {'innerArmLength':innerArmLength, 'innerArmAngleRad':0, 'innerArmAngleDeg':0, 'innerArmChannel':innerArmChannel,
+arms = {'innerArmLength':innerArmLength, 'innerArmAngleRad':0, 'innerArmAngleDeg':0, 'innerArmCHannel':innerArmChannel,
             'innerArmActuationRange':innerArmActuationRange, 'innerArmMinPulse':innerArmMinPulse,
-       'outerArmLength':outerArmLength, 'outerArmAngleRad':0, 'innerArmAngleDeg':0, 'outerArmChannel':outerArmChannel,
+       'outerArmLength':outerArmLength, 'outerArmAngleRad':0, 'innerArmAngleDeg':0, 'outerArmCHannel':outerArmChannel,
             'outerArmActuationRange':outerArmActuationRange, 'outerArmMinPulse':outerArmMinPulse,
        'penUpAngle':penUpAngle, 'penDownAngle':penDownAngle,'penChannel':penChannel,
        'armLength': innerArmLength + outerArmLength}
 
-image = {'inputFilename':"", 'outputFilename':outputFilename, 'treshold':treshold,
+image = {'inputFilename':"", 'outputFilename':outputFilename, 'scale':imageScale,
          'originX':originX, 'originY':originY, 'edgeAlgorithm':edgeAlgorithm,
-         'skipProcessing':skipProcessing, 'foundNextPixel':True, 'foundLastPixel':False}
+         'treshold':treshold,'skipProcessing':skipProcessing}
 
-raspi = {'switchedOn':raspiSwitchedOn, 'frequency':frequency, 'waitTimeNear':waitTimeNear, 'waitTimeNew':waitTimeNew, 'waitTimePen':waitTimePen}
+raspi = {'frequency':frequency, 'waitTimeNear':waitTimeNear, 'waitTimeNew':waitTimeNew, 'waitTimePen':waitTimePen}
 
-simulation = {'switchedOn':simulatorSwitchedOn, 'browser':browser, 'sizeX':sizeX, 'sizeY':sizeY, 'penWidth':penWidth}
+simulation = {'browser':browser, 'sizeX':sizeX, 'sizeY':sizeY, 'penWidth':penWidth}
 
 ### parts of browser window:
 # header
@@ -95,7 +92,7 @@ def settingsTab():
     settingsOuterArmLength = TextInput(value=str(outerArmLength), title="Outer arm length:")
     settingsOriginX = TextInput(value=str(originX), title="Origin x:")
     settingsOriginY = TextInput(value=str(originY), title="Origin y:")
-    settingsImageScale = TextInput(value=str(imageScaleFactor), title="Image scale:")
+    settingsImageScale = TextInput(value=str(imageScale), title="Image scale:")
     info = column(header, row(settingsInnerArmLength, settingsOuterArmLength), row(settingsOriginX, settingsOriginY), settingsImageScale, headerSimulator)
     #show(widgetbox(div))
     settings = Panel(child = info, title = "Settings")
@@ -106,69 +103,62 @@ def infoRaspi():
     tabRaspi = Panel(child = info, title = "RaspberryPi Info")
     return tabRaspi
 
-### drawing functions
-def drawLine(image, arms, raspi):
-    #image['foundNextPixel'] = False
-    helper.findPixel2(image)
-    #print('currentXYInArray:',image['currentXInArray'], image ['currentYInArray'])
-    if image['foundNextPixel']:
-        image['currentLineX'] = [image['currentX']]
-        image['currentLineY'] = [image['currentY']]
-        helper.getAngles2(image, arms)
-        #print('currentXYInArray:',image['currentXInArray'],image['currentYInArray'],'currentXY:',image['currentX'],image['currentY'])
-        if arms['innerArmAngleDeg'] != 0 and arms['outerArmAngleDeg'] != 0:
-            if simulation['switchedOn']:
-                simulator.newsimulator.moveArms(arms, simulation)
-                #simulator.newsimulator.drawLine(arms, simulation, image)
-            if raspi['switchedOn']:
-                print('raspi should draw here!')
-            helper.findAdjacentPixel2(image)
-            while image['foundNextPixel']:
-                #sleep(0.1)
-                helper.getAngles2(image, arms)
-                if arms['innerArmAngleDeg'] != 0 and arms['outerArmAngleDeg'] != 0:
-                    if simulation['switchedOn']:
-                        image['currentLineX'].append(image['currentX'])
-                        image['currentLineY'].append(image['currentY'])
-                        simulator.newsimulator.moveArms(arms, simulation)
-                        #simulator.newsimulator.drawLine(arms, simulation, image)
-                    if raspi['switchedOn']:
-                        print('raspi should draw here!')
-                    helper.findAdjacentPixel2(image)
-            simulator.newsimulator.drawLine(arms, simulation, image)
-            #sleep(0.5)
-    else:
-        # hurray, we finished drawing
-        print("nothing left to draw!")
-        # delete variables in dictionary:
-        image['currentX'] = image['currentY'] = image['currentXInArray'] = image['currentYInArray'] = False
-        image['currentLineX'] = image['currentLineY'] = []
-        return
-
-def update():
-    drawLine(image, arms, raspi)
-
-
 ### populating image dictionary:
 image['array'] = imageProcessor.imageProcessor.imageAsArray(image['outputFilename'], image['treshold'])
-image['scale'] = imageScaleFactor*image['array'].shape[0]/arms['armLength']
 image['width'] = image['array'].shape[0]/image['scale']
 image['height'] = image['array'].shape[1]/image['scale']
 image['currentXInArray'] = image['currentYInArray'] = False
 
-
 ### setting up the browser window
 header = header()
 tab1 = settingsTab()
-tab2 = simulator.newsimulator.setupSimulation(simulation, image, arms)
+tab2, simulation['innerArmDataStream'], simulation['outerArmDataStream'], simulation['figure'] = simulator.newsimulator.setupSimulation(simulation, image, arms)
 tab3 = infoRaspi()
 tabs = Tabs(tabs = [tab2, tab1, tab3])
 curdoc().add_root(column(children=[header, tabs], sizing_mode='scale_width'))
 
-curdoc().add_periodic_callback(update, 30)
 ### drawing process starts here:
-# while not image['foundLastPixel']:
-#     drawLine(image, arms, raspi)
-#print('arms:',arms)
-#print('image:',image)
-#print('simulation:',simulation)
+image['currentXInArray'], image['currentYInArray'] = helper.findPixel(image['array'])
+print('currentXYInArray:',image['currentXInArray'], image ['currentYInArray'])
+
+
+
+### drawing process starts here:
+image['currentXInArray'], image['currentYInArray'] = helper.findPixel(image['array'])
+print('currentXYInArray:',image['currentXInArray'], image ['currentYInArray'])
+while True:
+    if image['currentXInArray']:
+        image['currentX'] = image['currentXInArray']/image['scale']+image['originX']
+        image['currentY'] = image['currentYInArray']/image['scale']+image['originY']
+        print('currentXY:',image['currentX'],image['currentY'])
+        image['currentLineX'] = [image['currentX']]
+        image['currentLineY'] = [image['currentY']]
+        arms['innerArmAngleRad'], arms['outerArmAngleRad'], arms['innerArmAngleDeg'], arms['outerArmAngleDeg'] = helper.getAngles(image['currentX'], image['currentY'], arms['innerArmLength'], arms['outerArmLength'])
+        print('inner and outer angle:',arms['innerArmAngleDeg'], arms['outerArmAngleDeg'])
+        if arms['innerArmAngleDeg'] != 0 and arms['outerArmAngleDeg'] != 0:
+            simulator.newsimulator.moveArms(arms, simulation)
+            simulator.newsimulator.drawLine(arms, simulation, image)
+            ax, ay = helper.findAdjacentPixel(image['array'],image['currentXInArray'],image['currentYInArray'])
+            image['currentX'] = ax/image['scale']+image['originX']
+            image['currentY'] = ay/image['scale']+image['originY']
+            while (ax):
+                arms['innerArmAngleRad'], arms['outerArmAngleRad'], arms['innerArmAngleDeg'], arms['outerArmAngleDeg'] = helper.getAngles(image['currentX'], image['currentY'], arms['innerArmLength'], arms['outerArmLength'])
+                print('inner and outer angle:',arms['innerArmAngleDeg'], arms['outerArmAngleDeg'])
+                if arms['innerArmAngleDeg'] != 0 and arms['outerArmAngleDeg'] != 0:
+                    image['currentLineX'].append(image['currentX'])
+                    image['currentLineY'].append(image['currentY'])
+                    simulator.newsimulator.moveArms(arms, simulation)
+                    simulator.newsimulator.drawLine(arms, simulation, image)
+                ax, ay = helper.findAdjacentPixel(image['array'],ax,ay)
+                image['currentX'] = ax/image['scale']+image['originX']
+                image['currentY'] = ay/image['scale']+image['originY']
+                print('ax,ay:',ax,ay)
+                print('currentLine:',image['currentLineX'],image['currentLineY'])
+    else:
+        print("Nothing left to draw!")
+
+
+### testing area
+arms['innerArmAngle'] = 70
+arms['outerArmAngle'] = 20
+simulator.newsimulator.moveArms(arms, simulation)
